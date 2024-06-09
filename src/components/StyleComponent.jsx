be0df6b9034f2d5dd25bc3edf9700856fb3c296b
minxng <minxng12@gmail.com>
2024-06-09 22:54:53 +0900

    추가

---
+++ src/components/StyleComponent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #3c5b69;
+  color: #b9eaff;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `}
+`;
+
+export default function StyleComponents() {
+  return (
+    <div>
+      <Container>
+        <Button>Normal button</Button>
+        <Button primary>Special button</Button>
+      </Container>
+    </div>
+  );
+}
